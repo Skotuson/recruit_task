bbaee0e6a1d06134911d5e1a113f66b91581a7e9
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-09-08 19:12:03 +0200

    dns initial commit

---
+++ dns_task/README.md
@@ -0,0 +1,2 @@
+# DNS Task
+## Řešení
---
+++ dns_task/dns.cpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <tuple>
+
+using Result = std::pair<uint16_t, int>;
+
+struct Data {
+
+};
+
+struct ECS {
+
+};
+
+Result Route ( Data & d, const ECS & ecs ) {
+
+}
+
+int main ( void ) {
+
+    return 0;
+}
