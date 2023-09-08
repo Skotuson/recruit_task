7625da0ebdda0d7ffc8d9d39b7f9c6e17cfdcf7c
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-09-08 21:34:07 +0200

    add signatures

--- dns_task/dns.cpp
+++ dns_task/dns.cpp
@@ -1,14 +1,26 @@
 #include <cstdint>
+#include <cassert>
+#include <string>
 #include <tuple>
 
 using Result = std::pair<uint16_t, int>;
 
-struct Data {
+struct TrieNode {
+    std::string             m_Val;
+    std::vector<TrieNode *> m_Children;
+};
 
+struct Data {
+    bool       Find   ( const ECS & ecs, 
+                        Result    & r );
+    bool       Insert ( const ECS & ecs );
+
+    TrieNode * m_TrieRoot = nullptr;
 };
 
 struct ECS {
-
+    ECS ( const std::string & ecs );
+    std::vector<std::string> m_Subnets;
 };
 
 Result Route ( Data & d, const ECS & ecs ) {
